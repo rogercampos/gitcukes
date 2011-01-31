ce4f9c7de369a4a9dba4bd30ffb595cef3c60b2d
Roger Campos <roger@itnig.net>
2011-01-31 14:46:05 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Gitcukes
+========
+
+Gitcukes is a cucumber feature editor, integrated with your Git repo with the
+help of [Grit](https://github.com/mojombo/grit).
+
+It provides an user friendly interface to play with your cukes, helping you to
+keep them in sync with your customer!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitcukes::Application.config.secret_token = '310382cbbd091a234ac7160997ba8bbf68d99c96b724978efc59c5be2bc52673a059d1dbf8b6fe9c5462f4766ece0c90c57f042dee21d9ad4e31c8f4c6a9a3d8'
